1f36227fe2d278b5592779fd11b39fd4ca49d015
stian88 <rodriguezjs88@gmail.com>
2022-12-15 13:18:22 -0300

    DataBase-ForeignKey
    Actualizar base DataBase-ForeignKey

--- Agenda/Migrations/20221215161616_DataBase-ForeignKey.Designer.cs
+++ Agenda/Migrations/20221215161616_DataBase-ForeignKey.Designer.cs
@@ -4,6 +4,7 @@ using Agenda.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Agenda.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221215161616_DataBase-ForeignKey")]
+    partial class DataBaseForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
